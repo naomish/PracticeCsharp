70a138fc1eeadf3a8862de51926073465311c5e1
naomish <naomishness@gmail.com>
2014-07-29 13:11:41 -0400

    added landing page for master projects and scoreCard. No real content for these yet. added links to github for mastery project code.

---
+++ AppLabs/MVCappLabs/MVCappLabs.Web/Controllers/ScoreCardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCappLabs.Web.Controllers
+{
+    public class ScoreCardController : Controller
+    {
+        //
+        // GET: /ScoreCard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
